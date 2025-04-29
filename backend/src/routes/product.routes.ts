d8bf0e2d1144dc764db022158926414a58f5b9ba
Vitor Martin Doja <vitor.doja@compasso.com.br>
2025-04-29 17:06:13 -0300

    feat: created product routes on routes/

---
+++ backend/src/routes/product.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import * as ProductController from '../controllers/product.controller';
+
+const router = Router();
+
+router.post('/', ProductController.validateProductData, ProductController.createProduct);
+router.get('/', ProductController.getProducts);
+router.get('/:id', ProductController.checkExistingProduct, ProductController.getProductById);
+router.put(
+  '/:id',
+  ProductController.validateProductData,
+  ProductController.checkExistingProduct,
+  ProductController.updateProduct
+);
+router.delete('/:id', ProductController.checkExistingProduct, ProductController.deleteProduct);
+
+export default router
