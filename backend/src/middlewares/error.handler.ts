95e7e75a09673c90508a8a74eb3ff8b3d7d057eb
Richard Wesley <wesleyrichard30@gmail.com>
2025-04-30 13:56:28 -0300

    refactor: update zod error handler

--- backend/src/middlewares/error.handler.ts
+++ backend/src/middlewares/error.handler.ts
@@ -3,8 +3,7 @@ import { ZodError } from 'zod';
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ZodError) {
-    const errors = err.errors.map((e: any) => e.message) as string[];
-    res.status(400).json({ success: false, error: { message: 'Validation error occurred', errors: errors } });
+    res.status(400).json({ success: false, error: { message: 'Validation error occurred', errors: err } });
     return;
   }
 
